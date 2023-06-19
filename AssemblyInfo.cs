61ef20d58d4c1c27d4779c725e6b458574ec97b3
crownsoft <130629588+whatsapp-filter@users.noreply.github.com>
2023-06-19 14:11:38 +0800

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("WhatsAppNETAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KR Software")]
+[assembly: AssemblyCopyright("Copyright © 2020-2023. Kamarudin")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("WhatsAppNETAPI")]
+[assembly: Guid("bd90b9cb-ad87-414b-8ab3-700d69a58fe9")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[module: SuppressIldasm]
